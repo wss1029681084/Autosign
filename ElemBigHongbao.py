855317db9cc0bdbe5dc615948539839be11fa076
wangshanshan <wangshanshan@noahwm.com>
2018-12-11 14:12:41 +0800

    延迟0.2s请求接口

--- ElemBigHongbao.py
+++ ElemBigHongbao.py
@@ -21,7 +21,7 @@ def gethongbao(user):
         data = {"user_id": 321606090, "latitude": 31.250972747802734, "longitude": 121.51678466796875}
         n = 0
 
-        time.sleep(0.5)
+        time.sleep(0.8)
         while n < 10:
             n = n + 1
             res2 = requests.post(url2, data, cookies=cookie2, headers=headers) \
