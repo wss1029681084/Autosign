54be42512f37a6f014feda0c59fa4e91de08d356
wangshanshan <wangshanshan@noahwm.com>
2018-12-12 17:23:40 +0800

    no

--- start.py
+++ start.py
@@ -10,7 +10,7 @@ scheduler = BlockingScheduler()
 #default用户
 scheduler.add_job(hongbao_default,'cron',hour=9,minute=59,second=59)
 scheduler.add_job(hongbao_default,'cron',hour=13,minute=59,second=59)
-scheduler.add_job(hongbao_default,'cron',hour=16,minute=59,second=8)
+scheduler.add_job(hongbao_default,'cron',hour=16,minute=59,second=59)
 
 
 print("开始执行"+datetime.datetime.now().strftime('%H:%M:%S.%f'))
